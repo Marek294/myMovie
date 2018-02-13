cfe74d28a63bc2fd555e30272610b464958e70e8
Marek294 <Markra5835@gmail.com>
2018-02-13 17:36:27 +0100

    Destroy Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import classes from './Footer.css';
 const Footer = () => {
     return (
         <footer className={classes.Footer}>
-            <p>&copy; 2018 MyMovie</p>
+            <p>&copy; 2018 MyMovie DESTROYED</p>
         </footer>
     );
 };
