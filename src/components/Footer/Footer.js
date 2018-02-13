670698be9faaca5d82b76a8d5521d96379a14bc2
Marek294 <Markra5835@gmail.com>
2018-02-13 17:37:54 +0100

    Footer fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import classes from './Footer.css';
 const Footer = () => {
     return (
         <footer className={classes.Footer}>
-            <p>&copy; 2018 MyMovie DESTROYED</p>
+            <p>&copy; 2018 MyMovie</p>
         </footer>
     );
 };
